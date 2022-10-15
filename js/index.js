92e0d3018d1b741ea9d3b8dd6ae5e3b49eed1752
Xovee <xovee@ieee.org>
2022-10-15 21:01:51 +0800

    update recruit

--- js/index.js
+++ js/index.js
@@ -24,9 +24,10 @@ $( document ).ready(function() {
 
   $('#recruit-reset').click(function() {
     $('.recruit-tag-list a').removeClass('primary');
-    $('.recruit-player-list a').hide();
-    $('.recruit-player-list br').hide();
-    $('.recruit-player-list hr').hide();
+    $('.recruit-player-list div').hide();
+    $('.recruit-player-tags a').hide();
+    $('.recruit-player-tags br').hide();
+    $('.recruit-player-tags hr').hide();
   })
 
   $('.recruit-tag-list a').click(function() {
@@ -54,17 +55,30 @@ $( document ).ready(function() {
     $.each(activePlayerList, function(index, value) {
       var player = value;
       var tags = [];
+      var playerProbs = [];
+
       $.each(activeTags, function(index, value) {
         if (recruit[value].includes(player)) {
           tags.push(value);
+          playerProbs.push(1 / recruit[value].length);
         }
       })
+
+      var rareTag = tags.reduce(function(a, b) {
+        return recruit[a].length <= recruit[b].length ? a : b;
+      })
+
+      // console.log(playerProbs);
+      
+      var maxProb = playerProbs.reduce((a, b) => Math.max(a, b), -Infinity);
+
       playerText = player;
       tagsText = "";
+      var maxProbText = "<div><a class='button primary'>" + "<span class='player-prob-tag'>" + maxProb.toLocaleString('en', {style: 'percent', minimumFractionDigits: 2}) + "</span> " + playerText + ""  + "<a class='button primary outline'>" + rareTag + "</a>" + "</a></div>";
       $.each(tags, function(index, value) {
         tagsText += "<a class='button primary outline'>" + value + "</a>"
       })
-      activePlayerCount.push([tags.length, playerText, tagsText, tags]);
+      activePlayerCount.push([maxProb, maxProbText, tags.length, playerText, tagsText,  tags]);
     })
 
     // count active tags number
@@ -78,25 +92,33 @@ $( document ).ready(function() {
       activeTagNumbers[value] = (activeTagNumbers[value] || 0) + 1;
     })
     var activeTagNameAndNumber = Object.entries(activeTagNumbers);
+
+    // display matched players
+    activePlayerCount.sort((a, b) => (a[0] < b[0]) ? 1 : -1);
+    $.each(Array.from(activePlayerCount), function(index, value) {
+      $('.recruit-player-list').append(value[1]);
+    })
+    
+    // console.log(activePlayerCount);
     
 
     // add players to the page
-    activeTagNameAndNumber.sort((a, b) => (a[1] < b[1]) ? 1 : -1);
-    $.each(activeTagNameAndNumber, function(index, value) {
-      if (value[1] > 1) {
-        $('.recruit-player-list').append("<a class='button primary'>" + value[0] + ": " + value[1] + "</a>");
-      } else {
-        $('.recruit-player-list').append("<a class='button primary outline'>" + value[0] + ": " + value[1] + "</a>");
-      }
-    })
+    // activeTagNameAndNumber.sort((a, b) => (a[1] < b[1]) ? 1 : -1);
+    // $.each(activeTagNameAndNumber, function(index, value) {
+    //   if (value[1] > 1) {
+    //     $('.recruit-player-list').append("<a class='button primary'>" + value[0] + ": " + value[1] + "</a>");
+    //   } else {
+    //     $('.recruit-player-list').append("<a class='button primary outline'>" + value[0] + ": " + value[1] + "</a>");
+    //   }
+    // })
 
-    $('.recruit-player-list').append('<hr>');
+    // $('.recruit-player-list').append('<hr>');
 
-    activePlayerCount.sort((a, b) => (a[0] < b[0]) ? 1 : -1);
+    
     $.each(Array.from(activePlayerCount), function(index, value) {
       var addPlayers = [];
-      addPlayers.push( "<a class='button primary'>" + value[1] + "</a>" + value[2] + "<br>"); 
-      $('.recruit-player-list').append(addPlayers.join( "" ));
+      addPlayers.push( "<a class='button primary'>" + value[3] + "</a>" + value[4] + "<br>"); 
+      $('.recruit-player-tags').append(addPlayers.join( "" ));
 
     })
     
